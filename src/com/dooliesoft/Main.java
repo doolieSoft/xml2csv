484a274778eb499c6f274e574efbde6010f47434
stefano on the road <s.crapanzano@gmail.com>
2018-01-23 23:21:18 +0100

    1 minutes for 17000 xml invoices

--- src/com/dooliesoft/Main.java
+++ src/com/dooliesoft/Main.java
@@ -10,73 +10,21 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Main {
 
     //public static final String FOLDER = "C:\\Users\\c158492\\Desktop\\CREDIT NOTE\\ZHUL\\Nouveau Dossier";
-    //public static final String FOLDER = "C:\\Users\\fabrice\\Downloads\\Lot2";
-    public static final String FOLDER = "C:\\Users\\fabrice\\Downloads\\Lot2\\New folder";
-    public static final String CSV_SENDER_ID = "SenderID";
-    public static final String CSV_MESSAGE_REFERENCE_NUMBER = "MessageReferenceNumber";
-    public static final String CSV_DOCUMENT_DATE = "DocumentDate";
-    public static final String CSV_DOCUMENT_NUMBER = "DocumentNumber";
-    public static final String CSV_PARTNER_ID = "PartnerID";
-    public static final String CSV_BANK_ACCOUNT = "BankAccount";
-    public static final String CSV_NAME = "Name";
-    public static final String CSV_REFERENCE_NUMBER = "ReferenceNumber";
-    public static final String CSV_PATH = "Path";
-    public static final String CSV_DOCUMENT_ID = "DocumentID";
-    public static final String CSV_FILENAME = "Filename";
-    public static final String CSV_TOTAL_INVOICE_AMOUNT = "TotalInvoiceAmount";
-    public static final String CSV_TOTAL_VAT_AMOUNT = "TotalVATAmount";
-    public static final String CSV_TOTAL_NET_LINE_AMOUNT = "TotalNetLineAmount";
-    public static final String XML_SENDER_ID = "/Document/SenderID[1]";
-    public static final String XML_MESSAGE_REFERENCE_NUMBER = "/Document/Invoice[1]/MessageReferenceNumber[1]";
-    public static final String XML_INVOICE_DATE = "/Document/Invoice[1]/InvoiceHeader[1]/Dates[1]";
-    public static final String XML_DOCUMENT_NUMBER = "/Document/DocumentNumber[1]";
-    public static final String XML_PARTNER_ID = "/Document/Invoice[1]/InvoiceHeader[1]/Partners[1]/Partner[2]/PartnerID[1]";
-    public static final String XML_BANK_ACCOUNT = "/Document/Invoice[1]/InvoiceHeader[1]/Partners[1]/Partner[2]/BankAccount[1]";
-    public static final String XML_NAME = "/Document/Invoice[1]/InvoiceHeader[1]/Partners[1]/Partner[2]/Name[1]";
-    public static final String XML_REFERENCE_NUMBER = "/Document/Invoice[1]/InvoiceHeader[1]/Partners[1]/Partner[2]/PartnerReferences[1]/ReferenceNumber[1]";
-    public static final String XML_PATH = "/Document/Invoice[1]/InvoiceHeader[1]/CustomAddendum[1]/MetaData[1]/ScanParty[1]/Document[1]/Path[1]";
-    public static final String XML_DOCUMENT_ID = "/Document/Invoice[1]/InvoiceHeader[1]/CustomAddendum[1]/MetaData[1]/ScanParty[1]/Document[1]/DocumentID[1]";
-    public static final String XML_FILENAME = "/Document/Invoice[1]/InvoiceHeader[1]/CustomAddendum[1]/MetaData[1]/ScanParty[1]/Document[1]/Filename[1]";
-    public static final String XML_INVOICE_AMOUNT = "/Document/Invoice[1]/InvoiceTotals[1]/TotalInvoiceAmount[1]";
-    public static final String XML_TOTAL_VATAMOUNT = "/Document/Invoice[1]/InvoiceTotals[1]/TotalVATAmount[1]";
-    public static final String XML_TOTAL_NET_LINE_AMOUNT = "/Document/Invoice[1]/InvoiceTotals[1]/TotalNetLineAmount[1]";
-    public static final String XML_MESSAGE_NUMBER = "/Document/Invoice[1]/InvoiceHeader[1]/MessageNumber[1]";
-    public static final String XML_DOCUMENT_DATE = "/Document/Invoice[1]/InvoiceHeader[1]/CustomAddendum[1]/MetaData[1]/ScanParty[1]/Document[1]/DocumentDate[1]";
-    public static final String XML_MESSAGE_TYPE= "/Document/Invoice[1]/InvoiceHeader[1]/MessageType[1]";
-
-    private static final String KEY_SENDER_ID = "SenderID";
-    private static final String KEY_REFERENCE_NUMBER = "ReferenceNumber";
-    private static final String KEY_DOCUMENT_DATE = "DocumentDate";
-    private static final String KEY_DOCUMENT_NUMBER = "DocumentNumber";
-    private static final String KEY_PARTNER_ID = "PartnerID";
-    private static final String KEY_BANK_ACCOUNT = "BankAccount";
-    private static final String KEY_NAME = "Name";
-    private static final String KEY_PATH = "Path";
-    private static final String KEY_DOCUMENT_ID = "DocumentID";
-    private static final String KEY_FILENAME = "Filename";
-    private static final String KEY_TOTAL_INVOICE_AMOUNT = "TotalInvoiceAmount";
-    private static final String KEY_TOTAL_VAT_AMOUNT = "TotalVATAmount";
-    private static final String KEY_TOTAL_NET_LINE_AMOUNT = "TotalNetLineAmount";
-    private static final String KEY_MESSAGE_NUMBER = "MessageNumber";
-    private static final String KEY_INVOICE_DATE= "InvoideDate";
-    private static final String KEY_MESSAGE_TYPE= "MessageType";
-
-    public static Xml2CsvMapper xml2CsvMapper;
-    public static HashMap<String, String> xpathMap = new HashMap<>();
+    public static final String FOLDER = "C:\\Users\\fabrice\\Downloads\\Lot2";
+    //public static final String FOLDER = "C:\\Users\\fabrice\\Downloads\\Lot2\\New folder";
+    public static final LinkedHashMap<String, String> column_to_xml_xpath = new LinkedHashMap<>();
 
     public static void main(String[] args) throws IOException {
 
-        initializeHeaders();
+        loadParameters();
 
         ArrayList<Invoice> invoices = new ArrayList<>();
 
@@ -88,27 +36,19 @@ public class Main {
                     File fXmlFile = fileEntry.getAbsoluteFile();
 
                     Invoice draftInvoice = new Invoice();
+                    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                    Document doc = dBuilder.parse(fXmlFile);
+                    Element root = doc.getDocumentElement();
+                    XPathFactory xpf = XPathFactory.newInstance();
+                    XPath path = xpf.newXPath();
 
-                    draftInvoice.set(KEY_SENDER_ID, getValue(fXmlFile, KEY_SENDER_ID));
-                    draftInvoice.set(KEY_REFERENCE_NUMBER, getValue(fXmlFile, KEY_REFERENCE_NUMBER));
-                    draftInvoice.set(KEY_INVOICE_DATE, getValue(fXmlFile, KEY_INVOICE_DATE));
-                    draftInvoice.set(KEY_DOCUMENT_NUMBER, getValue(fXmlFile, KEY_DOCUMENT_NUMBER));
-                    draftInvoice.set(KEY_PARTNER_ID, getValue(fXmlFile, KEY_PARTNER_ID));
-                    draftInvoice.set(KEY_BANK_ACCOUNT, getValue(fXmlFile, KEY_BANK_ACCOUNT));
-                    draftInvoice.set(KEY_NAME, getValue(fXmlFile, KEY_NAME));
-                    draftInvoice.set(KEY_REFERENCE_NUMBER, getValue(fXmlFile, KEY_REFERENCE_NUMBER));
-                    draftInvoice.set(KEY_PATH, getValue(fXmlFile, KEY_PATH));
-                    draftInvoice.set(KEY_DOCUMENT_ID, getValue(fXmlFile, KEY_DOCUMENT_ID));
-                    draftInvoice.set(KEY_FILENAME, getValue(fXmlFile, KEY_FILENAME));
-                    draftInvoice.set(KEY_TOTAL_INVOICE_AMOUNT, getValue(fXmlFile, KEY_TOTAL_INVOICE_AMOUNT));
-                    draftInvoice.set(KEY_TOTAL_VAT_AMOUNT, getValue(fXmlFile, KEY_TOTAL_VAT_AMOUNT));
-                    draftInvoice.set(KEY_TOTAL_NET_LINE_AMOUNT, getValue(fXmlFile, KEY_TOTAL_NET_LINE_AMOUNT));
-                    draftInvoice.set(KEY_MESSAGE_NUMBER, getValue(fXmlFile, KEY_MESSAGE_NUMBER));
-                    draftInvoice.set(KEY_DOCUMENT_DATE, getValue(fXmlFile, KEY_DOCUMENT_DATE));
-                    draftInvoice.set(KEY_MESSAGE_TYPE, getValue(fXmlFile, KEY_MESSAGE_TYPE));
+                    for (Map.Entry<String, String> entry : column_to_xml_xpath.entrySet()) {
+                        String xpath_evaluated = getValue(fXmlFile, entry.getValue(), path, root);
+                        draftInvoice.set(entry.getKey(), xpath_evaluated);
+                    }
                     invoices.add(draftInvoice);
                 }
-
             } catch (ParserConfigurationException e) {
                 e.printStackTrace();
             } catch (IOException e) {
@@ -119,132 +59,60 @@ public class Main {
                 e.printStackTrace();
             }
         }
-
-//        for (Invoice invoice : invoices) {
-//            System.out.println(invoice.get(KEY_SENDER_ID));
-//            System.out.println(invoice.get(KEY_REFERENCE_NUMBER));
-//            System.out.println(invoice.get(KEY_DOCUMENT_DATE));
-//            System.out.println(invoice.get(KEY_DOCUMENT_NUMBER));
-//            System.out.println(invoice.get(KEY_PARTNER_ID));
-//            System.out.println(invoice.get(KEY_BANK_ACCOUNT));
-//            System.out.println(invoice.get(KEY_NAME));
-//            System.out.println(invoice.get(KEY_REFERENCE_NUMBER));
-//            System.out.println(invoice.get(KEY_PATH));
-//            System.out.println(invoice.get(KEY_DOCUMENT_ID));
-//            System.out.println(invoice.get(KEY_FILENAME));
-//            System.out.println(invoice.get(KEY_TOTAL_INVOICE_AMOUNT));
-//            System.out.println(invoice.get(KEY_TOTAL_VAT_AMOUNT));
-//            System.out.println(invoice.get(KEY_TOTAL_NET_LINE_AMOUNT));
-//        }
-
         BufferedWriter writer = new BufferedWriter(new FileWriter("test.csv"));
-        writer.write(KEY_SENDER_ID);
-        writer.write(";");
-        writer.write(KEY_REFERENCE_NUMBER);
-        writer.write(";");
-        writer.write(KEY_DOCUMENT_DATE);
-        writer.write(";");
-        writer.write(KEY_DOCUMENT_NUMBER);
-        writer.write(";");
-        writer.write(KEY_PARTNER_ID);
-        writer.write(";");
-        writer.write(KEY_BANK_ACCOUNT);
-        writer.write(";");
-        writer.write(KEY_NAME);
-        writer.write(";");
-        writer.write(KEY_REFERENCE_NUMBER);
-        writer.write(";");
-        writer.write(KEY_PATH);
-        writer.write(";");
-        writer.write(KEY_DOCUMENT_ID);
-        writer.write(";");
-        writer.write(KEY_FILENAME);
-        writer.write(";");
-        writer.write(KEY_TOTAL_INVOICE_AMOUNT);
-        writer.write(";");
-        writer.write(KEY_TOTAL_VAT_AMOUNT);
-        writer.write(";");
-        writer.write(KEY_TOTAL_NET_LINE_AMOUNT);
-        writer.write(";");
-        writer.write(KEY_MESSAGE_NUMBER);
-        writer.write(";");
-        writer.write(KEY_INVOICE_DATE);
-        writer.write(";");
-        writer.write(KEY_MESSAGE_TYPE);
-        writer.write("\n");
+
+        String toWrite = "";
+        for (String key : column_to_xml_xpath.keySet()) {
+            toWrite += key + ";";
+        }
+        writer.write(toWrite.substring(0, toWrite.length() - 1) + "\n");
 
         for (Invoice invoice : invoices) {
-            writer.write(invoice.get(KEY_SENDER_ID));
-            writer.write(";");
-            writer.write(invoice.get(KEY_REFERENCE_NUMBER));
-            writer.write(";");
-            writer.write(invoice.get(KEY_DOCUMENT_DATE));
-            writer.write(";");
-            writer.write(invoice.get(KEY_DOCUMENT_NUMBER));
-            writer.write(";");
-            writer.write(invoice.get(KEY_PARTNER_ID));
-            writer.write(";");
-            writer.write(invoice.get(KEY_BANK_ACCOUNT));
-            writer.write(";");
-            writer.write(invoice.get(KEY_NAME));
-            writer.write(";");
-            writer.write(invoice.get(KEY_REFERENCE_NUMBER));
-            writer.write(";");
-            writer.write(invoice.get(KEY_PATH));
-            writer.write(";");
-            writer.write(invoice.get(KEY_DOCUMENT_ID));
-            writer.write(";");
-            writer.write(invoice.get(KEY_FILENAME));
-            writer.write(";");
-            writer.write(invoice.get(KEY_TOTAL_INVOICE_AMOUNT));
-            writer.write(";");
-            writer.write(invoice.get(KEY_TOTAL_VAT_AMOUNT));
-            writer.write(";");
-            writer.write(invoice.get(KEY_TOTAL_NET_LINE_AMOUNT));
-            writer.write(";");
-            writer.write(invoice.get(KEY_MESSAGE_NUMBER));
-            writer.write(";");
-            writer.write(invoice.get(KEY_INVOICE_DATE));
-            writer.write(";");
-            writer.write(invoice.get(KEY_MESSAGE_TYPE));
-            writer.write("\n");
+            toWrite = "";
+            for (String key : column_to_xml_xpath.keySet()) {
+                toWrite += invoice.get(key) + ";";
+            }
+            writer.write(toWrite.substring(0, toWrite.length() - 1) + "\n");
         }
         writer.close();
     }
 
-    private static void initializeHeaders() {
+    private static void loadParameters() {
+        String csvFile = "parameter.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ";";
+        int i = 0;
+        try {
 
-        xml2CsvMapper = new Xml2CsvMapper();
-        xml2CsvMapper.map(KEY_SENDER_ID, XML_SENDER_ID, 1);
-        xml2CsvMapper.map(KEY_REFERENCE_NUMBER, XML_MESSAGE_REFERENCE_NUMBER, 2);
-        xml2CsvMapper.map(KEY_DOCUMENT_DATE, XML_INVOICE_DATE, 3);
-        xml2CsvMapper.map(KEY_DOCUMENT_NUMBER, XML_DOCUMENT_NUMBER, 4);
-        xml2CsvMapper.map(KEY_PARTNER_ID, XML_PARTNER_ID, 5);
-        xml2CsvMapper.map(KEY_BANK_ACCOUNT, XML_BANK_ACCOUNT, 6);
-        xml2CsvMapper.map(KEY_NAME, XML_NAME, 7);
-        xml2CsvMapper.map(KEY_REFERENCE_NUMBER, XML_REFERENCE_NUMBER, 8);
-        xml2CsvMapper.map(KEY_PATH, XML_PATH, 9);
-        xml2CsvMapper.map(KEY_DOCUMENT_ID, XML_DOCUMENT_ID, 10);
-        xml2CsvMapper.map(KEY_FILENAME, XML_FILENAME, 11);
-        xml2CsvMapper.map(KEY_TOTAL_INVOICE_AMOUNT, XML_INVOICE_AMOUNT, 12);
-        xml2CsvMapper.map(KEY_TOTAL_VAT_AMOUNT, XML_TOTAL_VATAMOUNT, 13);
-        xml2CsvMapper.map(KEY_TOTAL_NET_LINE_AMOUNT, XML_TOTAL_NET_LINE_AMOUNT, 14);
-        xml2CsvMapper.map(KEY_MESSAGE_NUMBER, XML_MESSAGE_NUMBER, 15);
-        xml2CsvMapper.map(KEY_INVOICE_DATE, XML_DOCUMENT_DATE, 16);
-        xml2CsvMapper.map(KEY_MESSAGE_TYPE, XML_MESSAGE_TYPE, 17);
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+                String[] parameter = line.split(cvsSplitBy);
+                if (i != 0) {
+                    column_to_xml_xpath.put(parameter[0], parameter[1]);
+                    System.out.println(parameter[0] + " , " + parameter[1]);
+                }
+                i++;
+            }
 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
-    private static String getValue(File fXmlFile, String key) throws XPathExpressionException, ParserConfigurationException, IOException, SAXException {
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-        Document doc = dBuilder.parse(fXmlFile);
-        Element root = doc.getDocumentElement();
-        XPathFactory xpf = XPathFactory.newInstance();
-        XPath path = xpf.newXPath();
-        String expression = xml2CsvMapper.getXMLPath(key);
+    private static String getValue(File fXmlFile, String key, XPath path, Element root) throws XPathExpressionException, ParserConfigurationException, IOException, SAXException {
+        String expression = key;
         String str = (String) path.evaluate(expression, root);
         return str.trim();
     }
 }
-
